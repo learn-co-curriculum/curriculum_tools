c2beb66ac3e2a74ec6e9d258192f3a51d1f456b5
Steven G. Harms <steven.harms@flatironschool.com>
2019-06-10 07:18:22 -0400

    Create output file

--- lesson_lists/ops_summary.sh
+++ lesson_lists/ops_summary.sh
@@ -3,6 +3,13 @@
 # Important note: Make sure you're using sgharms' fork of github/hub
 # so that you have the -r flag. Integration to github/hub is pending.
 
+process_sourcefile () {
+  cat "$1" |hub issue -r STDIN --include-pulls -f '%U%n' > "$2" 2>&1
+}
+
+SOURCE_FILES="prework_repos.txt mod1_repos.txt pfwtfp_repos.txt mod2_repos.txt mod3_1_repos.txt mod4_repos.txt v7_repos.txt"
+DEST_DIR_PREFIX=$(date "+%Y-%m-%d")
+
 if [ -z "$1" ]
 then
   SEARCH_DIR="${PWD}"
@@ -10,16 +17,18 @@ else
   SEARCH_DIR="$1"
 fi
 
-SOURCE_FILES="prework_repos.txt mod1_repos.txt pfwtfp_repos.txt mod2_repos.txt mod3_1_repos.txt mod4_repos.txt v7_repos.txt"
+mkdir $DEST_DIR_PREFIX
 
-process_sourcefile () {
-  cat "$1" |hub issue -r STDIN --include-pulls -f '%U%n' > "$2" 2>&1
-}
+if [ $? != 0 ]; then
+  echo "Unable to create ${DEST_DIR_PREFIX}. Exiting"
+  exit 1
+fi
 
 for source_file in $SOURCE_FILES
 do
   SRC="${SEARCH_DIR}/${source_file}"
   DEST="${SRC%_repos.txt}_issues.txt"
+  DEST=${DEST/$SEARCH_DIR/$DEST_DIR_PREFIX}
 
   if [ -f "${SRC}" ]
   then
@@ -29,5 +38,3 @@ do
     echo "Could not find ${SEARCH_DIR}/${source_file} | $SRC"
   fi
 done
-
-
