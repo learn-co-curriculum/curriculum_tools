0eff0933ab4f9c9b31c4218f9795af304ce67647
Steven G. Harms <steven.harms@flatironschool.com>
2019-08-01 10:13:04 -0400

    Support emitting list of GH urls

--- track_output.py
+++ track_output.py
@@ -5,6 +5,7 @@ import sys
 import urllib.request
 import json
 import pprint
+import re
 
 import pdb
 
@@ -13,6 +14,7 @@ parser = OptionParser()
 parser.add_option("-c", "--csv", dest="should_csv", action="store_true", help="Export track as CSV file")
 parser.add_option("-b", "--bullet", dest="should_bullet", action="store_true", help="Export track as Markdown-friendly bullet list with indentation")
 parser.add_option("-u", "--urls", dest="should_urlsonly", action="store_true", help="Export the track as a list of HTTP URLs")
+parser.add_option("-g", "--github-urls", dest="should_gh_urlsonly", action="store_true", help="Export the track as a list of Git URLs")
 (options, args) = parser.parse_args()
 
 # Ensure track ID
@@ -76,6 +78,18 @@ def only_urls(obj, indent=0):
             url = obj.get("github_url", "")
             print(url)
 
+def only_ghurls(obj, indent=0):
+    if "children" in obj:
+        if type(obj["children"]) is list and len(obj["children"]) > 0:
+            [only_ghurls(ch, indent + 2) for ch in obj["children"]]
+        elif type(obj["children"]) is list and len(obj["children"]) == 0:
+            url = obj.get("github_url", "")
+            print(frobnicate_repo_url_to_git_url(url))
+
+def frobnicate_repo_url_to_git_url(inp):
+    org_and_repo = re.sub(r'https?://github.com/', '', inp)
+    return 'git@github.com:' + org_and_repo + '.git'
+
 def quote_wrap(s):
     return '"' + s + '"'
 
@@ -89,6 +103,8 @@ try:
         bulletify(struc)
     elif options.should_urlsonly:
         only_urls(struc)
+    elif options.should_gh_urlsonly:
+        only_ghurls(struc)
     else:
         print(json.dumps(parse_obj_json(struc, {}), sort_keys=True, indent=4))
 except urllib.error.HTTPError as err:
