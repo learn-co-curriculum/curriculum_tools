c98b61f6c9dd57023b9f68dbc0738346e65f4c12
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-03 16:52:49 +0000

    Total screw up: Revert "Update cron script"
    This reverts commit ec83e29286b681d43421b12f4a1a51f93ee64664.
    
    Wow, I messed up bad. This script does the
    processing and I overwrote it with the script
    that's run out of cron to prep the OS and then to
    run ops_summary.sh.

--- ops_summary.sh
+++ ops_summary.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
-export OUTPUT_DIR=$(date '+%Y-%m-%d')
-cd $HOME/git_checkouts/curriculum-team/issues_output
-bash $HOME/git_checkouts/curriculum_tools/ops_summary.sh $HOME/git_checkouts/curriculum_tools/lesson_lists
-mkdir $OUTPUT_DIR
-mv $HOME/git_checkouts/curriculum_tools/lesson_lists/*_issues.md $OUTPUT_DIR
-pushd $OUTPUT_DIR
-wc -l * > summary.txt
-popd
-mv $OUTPUT_DIR $HOME/git_checkouts/curriculum-team/issues_output
-cd $HOME/git_checkouts/curriculum-team/issues_output/${OUTPUT_DIR}
-GIT_SSH_COMMAND="ssh -i $HOME/.ssh/automated-github" /usr/bin/git pull origin master
-git add .
-git commit -m "${OUTPUT_DIR} Automated Issue Tracking Results"
-GIT_SSH_COMMAND="ssh -i $HOME/.ssh/automated-github" /usr/bin/git push origin master > /dev/null 2>&1
-echo "Finished. Visit https://github.com/learn-co-curriculum/curriculum-team/tree/master/issues_output/${OUTPUT_DIR}" | mailx -s "Finished issues track update" se-academics@flatironschool.com > /dev/null 2>&1
+
+# Important note: Make sure you're using sgharms' fork of github/hub
+# so that you have the -r flag. Integration to github/hub is pending.
+
+if [ -z "$1" ]
+then
+  SEARCH_DIR="${PWD}"
+else
+  SEARCH_DIR="$1"
+fi
+
+SOURCE_FILES="prework_repos.txt mod1_repos.txt pfwtfp_repos.txt mod2_repos.txt mod3_1_repos.txt mod4_repos.txt v7_repos.txt"
+
+process_sourcefile () {
+  cat "$1" |/home/sgharms/git_checkouts/hub-sgh/bin/hub  issue -r STDIN -f '* %U%n' > "$2" 2>&1
+}
+
+for source_file in $SOURCE_FILES
+do
+  SRC="${SEARCH_DIR}/${source_file}"
+  DEST="${SRC%_repos.txt}_issues.md"
+
+  if [ -f "${SRC}" ]
+  then
+    process_sourcefile "${SEARCH_DIR}/${source_file}" "${DEST}"
+    echo "${source_file} $(wc -l ${DEST} |awk '{print $1}')"
+  else
+    echo "Could not find ${SEARCH_DIR}/${source_file} | $SRC"
+  fi
+done
